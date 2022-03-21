25e66666f5472cc8cd8b22fec517b20cf5de52ee
Tommaso Amici <me@tommasoamici.com>
2022-03-21 18:01:19 +0100

    refactor: use SolidJS for frontend

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import type { Component } from "solid-js";
+import Maplibre from "./components/Maplibre";
+
+const App: Component = () => {
+  return (
+    <>
+      <Maplibre />
+    </>
+  );
+};
+
+export default App;
